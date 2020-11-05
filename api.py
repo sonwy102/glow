d8a6d3049d410c2ddbf2d889588f39b97fc977a2
Wooyang Son <sonwy102@gmail.com>
2020-11-05 20:35:19 +0000

    add error handling

--- api.py
+++ api.py
@@ -6,15 +6,20 @@ import requests
 def get_all_products():
    
     res = requests.get('https://skincare-api.herokuapp.com/products')
-    products = res.json()
-
-    return products
+    
+    if res.status_code == 200:
+        print('product data successfully fetched!')
+        return res.json()
+    else:
+        return (f'Product data fetching failed. status code = {res.status_code}')
 
 def get_all_ingredients():
     res = requests.get('https://skincare-api.herokuapp.com/ingredients')
-    ingredients = res.json()
-
-    return ingredients
+    
+    if res.status_code == 200:
+        return res.json()
+    else:
+        return (f'Ingredient data fetching failed. status code = {res.status_code}'))
 
 def get_all_brands():
     
