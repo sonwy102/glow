600bea3e7da37c0dc39187fb3b495e94b30cfc4b
Wooyang Son <sonwy102@gmail.com>
2020-11-07 05:34:50 +0000

    validate user login

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from flask import (Flask, render_template, request, flash, session,
                    redirect)
 from model import connect_to_db
 import crud
+import jsonify
 
 from jinja2 import StrictUndefined
 
@@ -12,10 +13,28 @@ app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
+@app.route('/login')
 def index():
     """Show homepage."""
 
     return render_template('index.html')
+
+@app.route('/handle-login.json')
+def handle_login():
+    
+    email = request.args.get("email")
+    password = request.args.get("password")
+    user = crud.get_user_by_email(email)
+    
+    if user == None:
+        flash('Username does not exist.')
+        return redirect('/login')
+    if user.password != password:
+        flash('Incorrect username/password')
+        return redirect('/login')
+    
+    # session['user'] = user --> how to save login to session? or should i only save status
+    return 'OK'
 
 
 if __name__ == '__main__':
