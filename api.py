847b57cdc9ab9631b10fdae07ef081df138421dd
Wooyang Son <sonwy102@gmail.com>
2020-11-09 14:21:02 -0500

    add google image search api request

--- api.py
+++ api.py
@@ -1,6 +1,10 @@
 '''API helper functions'''
 
+import os
 import requests
+
+GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+GOOGLE_API_CX = os.environ['GOOGLE_CX']
 
 #SkincareAPI helper functions
 def get_all_products():
@@ -20,7 +24,7 @@ def get_all_ingredients():
         print('Ingredient data successfully fetched!')
         return res.json()
     else:
-        return (f'Ingredient data fetching failed. status code = {res.status_code}')
+        return (f'Ingredient data fetching failed - status code = {res.status_code}')
 
 def get_all_brands():
     
@@ -31,6 +35,34 @@ def get_all_brands():
         brands[product['brand']] = brands.get(product['brand'], 0) + 1
     
     return brands
-        
+
+def get_product_img(queryStr):
+
+    google_img_url = 'https://customsearch.googleapis.com/customsearch/v1'
+    payload = {
+        "key": GOOGLE_API_KEY,
+        "cx": GOOGLE_API_CX,
+        "searchType": "image",
+        "num": 1,
+        "q": queryStr}
+    
+    res = requests.get(google_img_url, params=payload)
+
+    if res.status_code == 200:
+        img_data = res.json()
+        if 'items' in img_data:
+            img_link = img_data['items'][0].get('link', '/static/img/product/default_product')
+        else:
+            img_link = '/static/img/product/default_product'
+        return img_link
+    elif res.status_code == 429:
+        raise RuntimeError(f'get_product_img failed - google api resource exhausted')
+    else:
+        raise RuntimeError(f'get_product_img failed - status code={res.status_code}')
+    
+    
+
+
+
 
 
