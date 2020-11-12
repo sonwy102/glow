077757604fee235f5bff5470f215f0c7e0ccce68
Wooyang Son <sonwy102@gmail.com>
2020-11-12 07:05:46 +0000

    add num_results

--- static/js/searchResults.jsx
+++ static/js/searchResults.jsx
@@ -1,21 +1,16 @@
 const SearchResults = () => {
-  //TODO: show a list of results returned from productSearch
-  //TODO 0: render when redirected from productSearch
-  //TODO 1: get result from productSearch.jsx
-  //TODO 2: format what the list will look like (just include name for now)
-  //TODO 3: return results in a list
+  // // TODO: show a list of results returned from productSearch
+  // // TODO 0: render when redirected from productSearch
+  // // TODO 1: get search terms from productSearch.jsx and query db for search terms
+  // // TODO 2: format what the list will look like (just include name for now)
+  // // TODO 3: return results in a list
   //TODO 4: link each item to product details page
+  
   const history = useHistory();
-//   const urlParams = useParams();
+  const [searchResults, setSearchResults] = React.useState([]);
   const urlParamsStr = history.location.search;
   const searchParams = new URLSearchParams(urlParamsStr)
 
-  
-
-
-
-  
-  
   const searchProduct = () => {
 
     const formData = {
@@ -25,13 +20,23 @@ const SearchResults = () => {
     console.log(formData);
 
     $.get("/product-search.json", formData, (res) => {
-      console.log(res);
+      setSearchResults(res)
     });
   };
 
-
+  React.useEffect(() => {
+    searchProduct(); 
+  }, []);
 
   return (
-      <div>hi</div>
-  )
+    <div className="search-results-page">
+      <div className="header">
+        <h3>Search Results</h3>
+        <div>Total: {searchResults.length} result(s)</div>
+        {searchResults.map((result) => (
+          <li>{result.name}</li>
+        ))}
+      </div>
+    </div>
+  );
 }
