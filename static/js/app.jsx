0d910ac0c3763f4e437eec13b902ea2ae5b35493
Wooyang Son <sonwy102@gmail.com>
2020-11-12 07:04:25 +0000

    add userState update function

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -4,12 +4,22 @@
 function App() {
     const [userState, setUserState] = React.useState(null)
     console.log(userState);
+
+    React.useEffect(() => {
+      const sess_id = localStorage.getItem("userState");
+      if (sess_id) {
+        setUserState(sess_id)
+      };
+    }, []);
+
     return (
       <Router>
         <div className="App">
           <h2 className="App-title">Glow Homepage</h2>
           <Link to="/">Home</Link>
-          <Link to="/login">Log In</Link>
+          <Link to="/login">Log In</Link> 
+          {/* TODO: how to dynamically change this link so it's Log In when user's 
+          logged out, and Log Out when user's logged in*/}
           <Link to="/register">Sign Up</Link>
         </div>
 
