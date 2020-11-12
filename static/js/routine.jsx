1d4a4b94769984cf94d1ac1303876dbb92ff4680
Wooyang Son <sonwy102@gmail.com>
2020-11-12 18:07:36 +0000

    add routine component

---
+++ static/js/routine.jsx
@@ -0,0 +1,21 @@
+// Routine Component for user to add a journal entry of their skincare routine
+
+// TODO: build a form with date, am/pm, products used, goals check-in
+// TODO: make a post request to db when the user submits form 
+    // TODO: make a flask server to handle post request
+    // TODO: create routine, routineProducts, userGoalEntry records in db
+// TODO: make component accessible only when user's logged in
+// TODO: set up appropriate routing in app.jsx and server.py and index.html
+
+const Routine = () => {
+    return (
+        <div className="routine-page">
+            <form className="routine-form">
+                <h2>Your routine today</h2>
+
+                
+
+            </form>
+        </div>
+    )
+}
