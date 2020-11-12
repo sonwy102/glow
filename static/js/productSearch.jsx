88d4e70014a0a7710efb56f967a87b25385f1654
Wooyang Son <sonwy102@gmail.com>
2020-11-12 06:08:34 +0000

    fix redirect issue

--- static/js/productSearch.jsx
+++ static/js/productSearch.jsx
@@ -1,5 +1,6 @@
-const ProductSearch = (props) => {
+const ProductSearch = () => {
   
+  const history = useHistory();
   const [searchState, setSearchState] = React.useState({
       'search-category': '',
       'product-search': null})
@@ -11,52 +12,54 @@ const ProductSearch = (props) => {
       setSearchState((prevState) => ({ ...prevState, [name]: value }));
   }
 
-  const searchProduct = (evt) => {
-      evt.preventDefault();
-      
-      const formData = {
-          "search_category": searchState['search-category'],
-          "product_search": searchState['product-search']
-      };
-      console.log(formData);
-
-      $.get('/product-search.json', formData, (res) => {
-        console.log(res);
-      });
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    history.push(
+      `/productsearch?category=${searchState["search-category"]}&term=${searchState["product-search"]}`
+    );
   }
 
   return (
-    <form onSubmit={searchProduct} className="product-search-form">
-      <div className="form-group">
-        <label htmlFor="search-category" className="sr-only">
-          Search Category
-        </label>
-        {/* ways to make this dropdown menu better? */}
-        <select id="search-category" name="search-category" onChange={handleInputChange} required>
-          <option value="DEFAULT" disabled selected>I'm looking for...</option>
-          <option value="Product">Product</option>
-          <option value="Brand">Brand</option>
-          <option value="Ingredient">Ingredient</option>
-        </select>
-      </div>
+    <div>
+      <form onSubmit={handleSubmit} className="product-search-form">
+        <div className="form-group">
+          <label htmlFor="search-category" className="sr-only">
+            Search Category
+          </label>
+          {/* ways to make this dropdown menu better? */}
+          <select
+            id="search-category"
+            name="search-category"
+            onChange={handleInputChange}
+            required
+          >
+            <option value="DEFAULT" disabled selected>
+              I'm looking for...
+            </option>
+            <option value="Product">Product</option>
+            <option value="Brand">Brand</option>
+            <option value="Ingredient">Ingredient</option>
+          </select>
+        </div>
 
-      <div className="form-group">
-        <label htmlFor="search-field" className="sr-only">
-          Product Search
-        </label>
-        <input
-          name="product-search"
-          className="form-control input-lg"
-          placeholder="Product name, brand, ingredients..."
-          required
-          autoFocus
-          onChange={handleInputChange}
-        ></input>
-      </div>
+        <div className="form-group">
+          <label htmlFor="search-field" className="sr-only">
+            Product Search
+          </label>
+          <input
+            name="product-search"
+            className="form-control input-lg"
+            placeholder="Product name, brand, ingredients..."
+            required
+            autoFocus
+            onChange={handleInputChange}
+          ></input>
+        </div>
 
-      <button className="btn btn-lg btn-primary btn-block" type="submit">
-        Search
-      </button>
-    </form>
+        <button className="btn btn-lg btn-primary btn-block" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
   );
 }
