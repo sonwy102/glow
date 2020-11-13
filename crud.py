40f3d82c88ff78438c3cfbe28ef98968fef16b87
Wooyang Son <sonwy102@gmail.com>
2020-11-13 17:49:56 -0500

    fix create_routine

--- crud.py
+++ crud.py
@@ -96,14 +96,6 @@ def create_user_goal_entry(user_goal_id, routine_id, goal_rating):
 
     return user_goal_entry
 
-# def get_latest_goal_entries(routine):
-#     """Return a list of user's latest goal entries"""
-
-#     latest_goal_entries = []
-#     for goal_entry in routine.usergoalentries:
-#         latest_goal_entries.append(goal_entry)
-    
-#     return latest_goal_entries
 
 def get_latest_goal_entries(routine):
     """return a list of user goal entries from the latest routine"""
@@ -111,9 +103,9 @@ def get_latest_goal_entries(routine):
     return UserGoalEntry.query.with_parent(routine).all()
     
 
-def create_routine(user_id, journal_date):
+def create_routine(user_id, journal_date, notes=None, photo=None):
 
-    routine = Routine(user_id=user_id, journal_date=journal_date)
+    routine = Routine(user_id=user_id, journal_date=journal_date, notes=notes, photo=photo)
 
     db.session.add(routine)
     db.session.commit()    
