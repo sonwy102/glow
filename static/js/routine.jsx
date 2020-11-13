e878687b38c16b1cd7aba9550e5e40e7447a1191
Wooyang Son <sonwy102@gmail.com>
2020-11-13 00:34:28 -0500

    pre-populate routine form

--- static/js/routine.jsx
+++ static/js/routine.jsx
@@ -1,14 +1,14 @@
 // Routine Component for user to add a journal entry of their skincare routine
 
 // // TODO: build a form with date, am/pm, products used, goals check-in
-// TODO: make a get request to db to pre-populate forms with latest values
+// // TODO: make a get request to db to pre-populate forms with latest values
 // TODO: make a post request to db when the user submits form 
     // TODO: make a flask server to handle post request
     // TODO: create routine, routineProducts, userGoalEntry records in db
 // TODO: make component accessible only when user's logged in
 // TODO: set up appropriate routing in app.jsx and server.py and index.html
 
-const Routine = () => {
+const Routine = (props) => {
 
   const today = new Date();
   const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
@@ -16,6 +16,21 @@ const Routine = () => {
   $(document).ready(function () {
     $(".js-basic-multiple").select2();
   });
+
+  const [latestProducts, setLatestProducts] = React.useState([]);
+  const [latestGoalRatings, setLatestGoalRatings] = React.useState([]);
+  
+  React.useEffect(() => {
+    $.get("/routine-products.json", { uid: props.isLoggedIn }, (res) => {
+      setLatestProducts(res);
+      console.log(latestProducts);
+    });
+
+    $.get("/goal-ratings.json", { uid: props.isLoggedIn }, (res) => {
+      setLatestGoalRatings(res);
+      console.log(latestGoalRatings);
+    });
+  }, []);
 
   return (
     <div className="routine-page">
@@ -64,53 +79,64 @@ const Routine = () => {
         <div className="form-group">
           <h3>Products you used today </h3>
 
-          {/* // TODO: render checkboxes of products from latest routine */}
           <select
             className="js-basic-multiple"
             name="products-used"
             multiple="multiple"
           >
-            <option value="test_product1">product1</option>
-            <option value="test_product2">product2</option>
+            {latestProducts.map((result) => (
+              <option value={result.product}>{result.product}</option>
+            ))}
           </select>
+
+          {/* // TODO: what about new products that aren't in previous routine? */}
         </div>
 
         <div className="form-group">
           <h3>Check in with your goals.</h3>
           <p>How are you feeling about your goals?</p>
 
-          {/* // TODO: dynamically display user's goals with ajax request to db 
-              // TODO: pre-populate range value with latest value */}
-
-          <label for="formControlRange">Goal1</label>
-          <input
-            type="range"
-            class="form-control-range"
-            id="formControlRange"
-          ></input>
-
-          <label for="formControlRange">Goal2</label>
-          <input
-            type="range"
-            class="form-control-range"
-            id="formControlRange"
-          ></input>
+          {/* // // TODO: dynamically display user's goals with ajax request to db 
+            // // TODO: pre-populate range value with latest value 
+            // TODO: add tickmarks and labels on range later
+            // TODO: add "update goals button" later  
+          */}
+          
+          {latestGoalRatings.map((result) => (
+            <React.Fragment>
+              <label htmlFor="formControlRange">{result.name}</label>
+              <input
+                type="range"
+                className="form-control-range"
+                id="formControlRange"
+                max="10"
+                defaultValue={result.rating}
+              ></input>
+            </React.Fragment>
+          ))}
         </div>
 
         <div className="form-group">
           <h3>Notes</h3>
 
-          <label for="notesTextArea"></label>
+          <label htmlFor="notesTextArea"></label>
           <textarea
-            class="form-control"
+            className="form-control"
             id="notes-text-area"
             rows="3"
           ></textarea>
 
-          <label for="photoFileInput">Photos</label>
-          <input type="file" class="form-control-file" id="photo-input"></input>
+          <label htmlFor="photoFileInput">Photos</label>
+          <input
+            type="file"
+            className="form-control-file"
+            id="photo-input"
+          ></input>
         </div>
 
+        <button className="btn btn-lg btn-primary btn-block" type="submit">
+            Save
+        </button>
       </form>
     </div>
   );
