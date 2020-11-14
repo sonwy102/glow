bb83198f7e83c21bbab40eabf3d850b6f60c91df
Wooyang Son <sonwy102@gmail.com>
2020-11-13 19:42:26 -0500

    add redirect to details

--- static/js/searchResults.jsx
+++ static/js/searchResults.jsx
@@ -4,7 +4,7 @@ const SearchResults = () => {
   // // TODO 1: get search terms from productSearch.jsx and query db for search terms
   // // TODO 2: format what the list will look like (just include name for now)
   // // TODO 3: return results in a list
-  //TODO 4: link each item to product details page
+  // // TODO 4: link each item to product details page
   
   const history = useHistory();
   const [searchResults, setSearchResults] = React.useState([]);
@@ -24,6 +24,11 @@ const SearchResults = () => {
     });
   };
 
+  const renderProductDetails = (evt) => {
+    evt.preventDefault();
+    history.push(`/details?category=${searchResults.category}&pid=${searchResults.id}`)
+  }
+
   React.useEffect(() => {
     searchProduct(); 
   }, []);
@@ -34,7 +39,9 @@ const SearchResults = () => {
         <h3>Search Results</h3>
         <div>Total: {searchResults.length} result(s)</div>
         {searchResults.map((result) => (
-          <li>{result.name}</li>
+          <li>
+            <a href='/details' onClick={renderProductDetails}>{result.name}</a>
+          </li>
         ))}
       </div>
     </div>
