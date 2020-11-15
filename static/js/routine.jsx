3f18224eb4c2c31d8760e96461b8f62c76555729
Wooyang Son <sonwy102@gmail.com>
2020-11-14 20:20:09 -0500

    handle goal check-in and submit

--- static/js/routine.jsx
+++ static/js/routine.jsx
@@ -2,9 +2,8 @@
 
 // // TODO: build a form with date, am/pm, products used, goals check-in
 // // TODO: make a get request to db to pre-populate forms with latest values
-// TODO: make a post request to db when the user submits form 
-    // TODO: make a flask server to handle post request
-    // TODO: create routine, routineProducts, userGoalEntry records in db
+// // TODO: make a post request to db when the user submits form 
+    // // TODO: make a flask server to handle post request
 // // TODO: make component accessible only when user's logged in
 // // TODO: set up appropriate routing in app.jsx and server.py and index.html
 
