e764db7ee59333aca1f0ebe4408c2f32d776d40f
Wooyang Son <sonwy102@gmail.com>
2020-11-16 16:46:42 -0500

    fix <form>

--- static/js/editProfile.jsx
+++ static/js/editProfile.jsx
@@ -16,37 +16,26 @@ const EditProfile = (props) => {
     history.push("/login");
   }
   return (
-    <div className="edit-user-profile-page">
-      <Form>
-        <div className="profile-photo">
-          {/* <img src={userDetails[0].photo}></img> */}
-          Profile photo
-        </div>
-        <div className="user-info-1">
-          <h3>Name</h3>
-          {/* <h3>{userDetails[0].name}</h3> */}
-          <p>Email</p>
-          {/* <p>{userDetails[0].email}</p> */}
-        </div>
-        <div className="user-info-2">
-          <h3>Skin Types</h3>
-          <Select
-            className="js-basic-multiple"
-            name="skinTypes"
-            isMulti
-            options={}
-            onChange={handleProductChange}
-          ></Select>
-          <div className="goal-btns">
-            <h3>Skin Health Goals You are Working On:</h3>
-            <Button variant="outline-primary goal_unselected">Goal 1</Button>
-            <Button variant="outline-primary goal_unselected">Goal 2</Button>
-          </div>
-        </div>
-        <Button variant="primary" onClick={redirectToProfile}>
-          Save
-        </Button>
-      </Form>
+    <div className="edit-profile-page">
+      <div className="formBasicUserInfo">
+        <h3>Name</h3>
+        <div>Email</div>
+      </div>
+
+      <div className="skin-types">
+        <h3>Skin Types</h3>
+        <Button>Oily</Button>
+      </div>
+
+      <div className="skin-goals">
+        <h3>Skin Health Goals You are Working On:</h3>
+        <Button variant="outline-primary goal_unselected">Goal 1</Button>
+        <Button variant="outline-primary goal_unselected">Goal 2</Button>
+      </div>
+
+      <Button variant="primary" onClick={redirectToProfile}>
+        Save
+      </Button>
     </div>
   );
-};
+}
