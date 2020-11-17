033df066bd9fe8973f83aa11af2c0163b2666b6c
Wooyang Son <sonwy102@gmail.com>
2020-11-16 21:34:47 -0500

    add chart component

---
+++ static/js/chart.jsx
@@ -0,0 +1,26 @@
+const Chart = () => {
+  
+  const chartConfig = {
+    type: 'bar',
+    data: {
+      labels: ['a', 'b', 'c'],
+      datasets: [
+        {data: [2,4,6]}
+      ]
+    }
+  }
+  const chartContainer = useRef(null);
+  const [chartInstance, setchartInstance] = useState(null);
+
+  // useEffect(() => {
+  //   if (chartContainer && chartContainer.current) {
+  //     const newChartInstance = new Chart()
+  //   }
+  // })
+
+  const newChartInstance = new Chart(chartContainer.current, )
+  return (
+    <canvas ref={chartContainer}></canvas>
+
+  )
+}
