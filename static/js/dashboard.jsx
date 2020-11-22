2c1bef671d3285e8fa8e899953ff20c0c315c4d8
Wooyang Son <sonwy102@gmail.com>
2020-11-22 05:15:51 +0000

    fetch user-highlights and render on page

--- static/js/dashboard.jsx
+++ static/js/dashboard.jsx
@@ -6,16 +6,39 @@
 // TODO: configure URL appropriately
 
 const Dashboard = (props) => {
+  const [productHighlight, setProductHighlight] = React.useState({});
+  const [daysHighlight, setDaysHighlight] = React.useState({});
+  const [goalsHighlight, setGoalsHighlight] = React.useState({});
+
+  const fetchUserHighlights = async () => {
+    fetch(`/get-highlights/${props.isLoggedIn}`)
+    .then((response) => response.json())
+    .then((data) => {
+      setProductHighlight(data.productHighlight);
+      setDaysHighlight(data.daysHighlight);
+      setGoalsHighlight(data.goalsHighlight);
+    });
+  }
   
-  
+  React.useEffect(() => {
+    fetchUserHighlights();
+  }, []);
+
   return (
     <div className="dashboard-page">
+      <div className="header">My Dashboard</div>
       <div className="highlights-section">
-
+        <div className="product-highlight">
+          {productHighlight.product_count} products in my routine
+        </div>
+        <div className="days-highlight">
+          {daysHighlight.product_count} days of skincare
+        </div>
+        <div className="goal-highlight">
+          {goalsHighlight.product_count} skin health goals
+        </div>
       </div>
-      <div className="chart-section">
-
-      </div>
+      <div className="chart-section"></div>
     </div>
-  )
+  );
 }
