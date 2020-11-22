36100189bfbe8ce7ed755f2f0ee1aed5293b5e55
Wooyang Son <sonwy102@gmail.com>
2020-11-22 03:10:45 +0000

    set up dashboard component

---
+++ static/js/dashboard.jsx
@@ -0,0 +1,21 @@
+// Dashboard component
+// TODO: make it accessible to logged in user only
+// TODO: render next to (to the right of) profile
+// TODO: make get request to server to fetch data trends
+// TODO: render progress chart with chart.js
+// TODO: configure URL appropriately
+
+const Dashboard = (props) => {
+  
+  
+  return (
+    <div className="dashboard-page">
+      <div className="highlights-section">
+
+      </div>
+      <div className="chart-section">
+
+      </div>
+    </div>
+  )
+}
