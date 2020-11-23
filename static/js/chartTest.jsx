422f67e5a71d3d35e40459230f49219804b2d4e7
Wooyang Son <sonwy102@gmail.com>
2020-11-23 00:45:33 -0500

    rename obscure vars

--- static/js/chartTest.jsx
+++ static/js/chartTest.jsx
@@ -1,4 +1,9 @@
 // Chart component to track goal-ratings trends over time
+// TODO: get code review on data requesting/handling/processing/plotting
+// TODO: integrate with profile eventually
+// TODO: plot monthtly graph + render when 'This Month' button is clicked
+// TODO: plot yearly graph + render when 'This Year' button is clicked
+// TODO: fix weekly chart - only showing 3 days worth of data for now
 const RatingsChart = (props) => {
 
   const [chartData, setChartData] = React.useState({});
@@ -10,17 +15,21 @@ const RatingsChart = (props) => {
     .then(response => response.json())
     .then(data => {
       const ratingsDatasets = [];
-      for (const [key, value] of Object.entries(data)) {
+      for (const [goalName, goalRatings] of Object.entries(data)) {
         ratingsDatasets.push({
-          label: key,
-          data: Object.values(value),
+          label: goalName,
+          data: Object.values(goalRatings),
           fill: true,
+
+          // TODO: fix this to generate different colors for each dataset dynamically
           backgroundColor: "rgba(75,192,192,0.2)",
           borderColor: "rgba(75,192,192,1)",
         });
       };
       const goalRatingsData = {
-        labels: Object.keys(data[3]),
+
+        // TODO: fix this to be dynamic
+        labels: Object.keys(data['Irritation']),
         datasets: ratingsDatasets
       }
       setChartData(goalRatingsData);
