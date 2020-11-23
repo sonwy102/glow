ad9d631d36f80202bb08232b562ce16b50cf38e4
Wooyang Son <sonwy102@gmail.com>
2020-11-23 17:40:33 -0500

    render month and year charts

--- static/js/ratingsChart.jsx
+++ static/js/ratingsChart.jsx
@@ -3,15 +3,17 @@
 // TODO: integrate with profile eventually
 // TODO: plot monthtly graph + render when 'This Month' button is clicked
 // TODO: plot yearly graph + render when 'This Year' button is clicked
-// TODO: fix weekly chart - only showing 3 days worth of data for now
+// // TODO: fix weekly chart - only showing 3 days worth of data for now
 const RatingsChart = (props) => {
 
   const [weekChartData, setWeekChartData] = React.useState({});
+  const [monthChartData, setMonthChartData] = React.useState({});
+  const [yearChartData, setYearChartData] = React.useState({});
 
   console.log('Week chart data: ', weekChartData);
 
   const fetchWeekRatings = async () => {
-    fetch(`/week-goal-ratings/${props.isLoggedIn}`)
+    fetch(`/goal-ratings-trend/${props.isLoggedIn}/10`)
     .then(response => response.json())
     .then(data => {
       const ratingsDatasets = [];
@@ -37,12 +39,64 @@ const RatingsChart = (props) => {
     });
   }
 
-  // const fetchMonthRatings = async () => {
+  const fetchMonthRatings = async () => {
+    fetch(`/goal-ratings-trend/${props.isLoggedIn}/31`)
+    .then(response => response.json())
+    .then(data => {
+      const ratingsDatasets = [];
+      const goalNames = [];
+      for (const [i, goalEntryData] of Object.entries(data)) {
+        goalNames.push(Object.keys(goalEntryData)[0]);
+        ratingsDatasets.push({
+          label: goalNames[i],
+          data: Object.values(goalEntryData[Object.keys(goalEntryData)[0]]),
+          fill: true,
 
-  // }
+          // TODO: fix this to generate different colors for each dataset dynamically
+          backgroundColor: "rgba(75,192,192,0.2)",
+          borderColor: "rgba(75,192,192,1)",
+        });
+      };
+      const goalRatingsData = {
+        // // TODO: fix this to be dynamic
+        labels: Object.keys(data[0][goalNames[0]]),
+        datasets: ratingsDatasets
+      }
+      setMonthChartData(goalRatingsData);
+    });
+  }
+
+  const fetchYearRatings = async () => {
+    fetch(`/goal-ratings-trend/${props.isLoggedIn}/365`)
+    .then(response => response.json())
+    .then(data => {
+      const ratingsDatasets = [];
+      const goalNames = [];
+      for (const [i, goalEntryData] of Object.entries(data)) {
+        goalNames.push(Object.keys(goalEntryData)[0]);
+        ratingsDatasets.push({
+          label: goalNames[i],
+          data: Object.values(goalEntryData[Object.keys(goalEntryData)[0]]),
+          fill: true,
+
+          // TODO: fix this to generate different colors for each dataset dynamically
+          backgroundColor: "rgba(75,192,192,0.2)",
+          borderColor: "rgba(75,192,192,1)",
+        });
+      };
+      const goalRatingsData = {
+        // // TODO: fix this to be dynamic
+        labels: Object.keys(data[0][goalNames[0]]),
+        datasets: ratingsDatasets
+      }
+      setYearChartData(goalRatingsData);
+    });
+  }
 
   React.useEffect(() => {
     fetchWeekRatings();
+    fetchMonthRatings();
+    fetchYearRatings();
   }, []);
 
   
@@ -54,6 +108,8 @@ const RatingsChart = (props) => {
     <div className="chart-section">
       <div>
         <Line data={weekChartData}></Line>
+        <Line data={monthChartData}></Line>
+        <Line data={yearChartData}></Line>
       </div>
 
       <div className="chart-view-btns">
