bea4b5f5890e3785ec2734de2cfe67b55faf7d27
Wooyang Son <sonwy102@gmail.com>
2020-12-02 00:17:13 -0500

    fix typo

--- server.py
+++ server.py
@@ -328,20 +328,14 @@ def get_user_highlights(user_id):
             else: 
                 products[product_name] = 1
             product_ings = crud.get_product_ing_by_product(product.id)
-            # for product_ing in product_ings:
-            #     ing = crud.get_ing_by_id(product_ing.ingredient_id) 
-            #     ing_lst.append(ing)
+            for product_ing in product_ings:
+                ing = crud.get_ing_by_id(product_ing.ingredient_id) 
+                ing_lst.append(ing)
     
-    # active_ing_lst = crud.get_active_ingredients(ing_lst)
-    # active_ings = {}
-    # for active_ing in active_ing_lst:
-    #     if active_ing.name in active_ings:
-    #         active_ings[active_ing.name] += 1
-    #     else:
-    #         active_ings[active_ing.name] = 1
+    active_ing_lst = crud.get_active_ingredients(ing_lst)
 
     res['productHighlight'] = {'product_count': len(products), 'product_data': products}
-    # res['ingHighlight'] = {'ing_count': len(active_ings), 'ing_data': active_ings}
+    res['ingHighlight'] = {'ing_count': len(active_ing_lst)}
     
     # Query how many days in a row user did a skincare routine
     # // TODO: fix routine_days unit issue - right now, it's how many 'routines in a row'
