80bbef91b87354f1e3bcb5a7def7e6db7971ccb2
Wooyang Son <sonwy102@gmail.com>
2020-12-02 03:36:52 -0500

    factor out user highlights

--- static/js/dashboard.jsx
+++ static/js/dashboard.jsx
@@ -6,42 +6,10 @@
 // // TODO: configure URL appropriately
 
 const Dashboard = (props) => {
-  const [productHighlight, setProductHighlight] = React.useState({
-    product_count: 0,
-    product_data: {'loading name': 'loading count...'},
-  });
-  const [daysHighlight, setDaysHighlight] = React.useState({
-    routine_count: 0,
-    routine_ratio: {'loading routine': 'loading routine ratio...'}
-  });
-  const [goalsHighlight, setGoalsHighlight] = React.useState({
-    goal_count: 0,
-    goal_data: ['loading goal data...']
-  });
-  const [ingHighlight, setIngHighlight] = React.useState({
-    ing_count: 0,
-    ing_data: { "loading name": "loading count..." }
-  });
-  const [ingTree, setIngTree] = React.useState({'data': 'loading...'});
   
+  const [ingTree, setIngTree] = React.useState({'data': 'loading...', 'children': 'loading...'});
+  console.log('ingTree: ', ingTree)
 
-  console.log('productHighLight: ', productHighlight);
-  console.log('product data: ', productHighlight.product_data);
-  console.log('goalsHighlight: ',goalsHighlight);
-  console.log("ingHighlight: ", ingHighlight);
-
-
-
-  const fetchUserHighlights = async () => {
-    fetch(`/get-highlights/${props.isLoggedIn}`)
-    .then((response) => response.json())
-    .then((data) => {
-      setProductHighlight(data.productHighlight);
-      setDaysHighlight(data.daysHighlight);
-      setGoalsHighlight(data.goalsHighlight);
-      setIngHighlight(data.ingHighlight);
-    });
-  };
 
   const fetchIngTree = async () => {
     fetch("/ingredient-analysis.json")
@@ -52,7 +20,6 @@ const Dashboard = (props) => {
   };
   
   React.useEffect(() => {
-    fetchUserHighlights();
     fetchIngTree();
   }, []);
   
@@ -72,43 +39,14 @@ const Dashboard = (props) => {
       <div className="header">
         <h2>My Dashboard</h2>
       </div>
-      <div className="highlights-section">
-        <div className="subheader">
-          <h4>Highlights</h4>
-        </div>
-        <div className="product-highlight">
-          {productHighlight.product_count} products in my routine
-          {Object.keys(productHighlight.product_data).map((productName) => (
-            <li>{productName}: {productHighlight.product_data[productName]}</li>
-          ))}
-        </div>
-            
-        <div className="ing-highlight">
-          {ingHighlight.ing_count} ingredients in my routine
-          {Object.keys(ingHighlight.ing_data).map((ingName) => (
-            <li>{ingName}: {ingHighlight.ing_data[ingName]}</li>
-          ))}
-        </div>
+      <div className="user-highlight">
+        <UserHighlights 
+          isLoggedIn={props.isLoggedIn} 
+          userInfo={props.userInfo}></UserHighlights>
+      </div>
 
-        <div className="ing-chart">
-          <IngredientChart data={ingTree} />
-        </div>
-
-        <div className="days-highlight">
-          {daysHighlight.routine_count} days of skincare
-          <RoutineRatioChart 
-            routineRatioKeys={Object.keys(daysHighlight.routine_ratio)}
-            routineRatioValues={Object.values(daysHighlight.routine_ratio)}
-          />
-        </div>
-
-        <div className="goal-highlight">
-          {goalsHighlight.goal_count} skin health goals
-          {goalsHighlight.goal_data.map((goal) => 
-            <li>{goal.name}</li>
-          )}
-
-        </div>
+      <div className="ing-chart">
+        <IngredientChart data={ingTree} />
       </div>
 
       <div className="chart-section">
