f9a0ae9dc6aa8f48a57edf5b678c30d335bd0e6e
Wooyang Son <sonwy102@gmail.com>
2020-12-03 01:06:11 -0500

    add styling

--- static/js/routineRatioChart.jsx
+++ static/js/routineRatioChart.jsx
@@ -1,17 +1,24 @@
 const RoutineRatioChart = (props) => {
   
   const data = {
-      labels: props.routineRatioKeys,
-      datasets: [
-        {
-          label: 'Routine',
-          data: props.routineRatioValues
-        }
-      ]
-  }
+    labels: props.routineRatioKeys,
+    datasets: [
+      {
+        label: "Routine",
+        data: props.routineRatioValues,
+        backgroundColor: ["#958079", "#442621"]
+      },
+    ],
+  };
+
+  const options = {
+    legend: {
+      position: "right",
+    },
+  };
   return (
     <div className="routine-ratio-chart">
-      <Doughnut data={data} />
+      <Doughnut data={data} options={options} />
     </div>
   );
 }
