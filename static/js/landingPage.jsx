7d9a03c277446b3c6a51e09c198710caa72c1dca
Wooyang Son <sonwy102@gmail.com>
2020-12-02 21:43:00 -0500

    add landing page

---
+++ static/js/landingPage.jsx
@@ -0,0 +1,34 @@
+const LandingPage = () => {
+  
+  return (
+    <Container fluid>
+      <Row className="landing-page">
+        <Col className="background">
+          <img
+            className="background"
+            width="100%"
+            srcset="https://res.cloudinary.com/sonwy102/image/upload/f_auto/v1606939001/glowImg/skin-small_p5mx9r.png 256w, 
+                    https://res.cloudinary.com/sonwy102/image/upload/f_auto/v1606939001/glowImg/skin-medium_elynjh.png 512w, 
+                    https://res.cloudinary.com/sonwy102/image/upload/f_auto/v1606939007/glowImg/skin-large_z146gw.png 1081w"
+            src="https://res.cloudinary.com/sonwy102/image/upload/v1606939007/glowImg/skin-medium_elynjh.png"
+          ></img>
+          <p>
+            <span className="header">
+              Start Your Journey towards Healthier Skin Today.
+              <p>
+                Say hello to Glow, a personalized goal and routine tracker to
+                truly understand your skin from within.
+              </p>
+              <Button href="/register" variant="light" size="lg">
+                Sign Up
+              </Button>
+              <Button href="/login" variant="light" size="lg">
+                Log In
+              </Button>
+            </span>
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
