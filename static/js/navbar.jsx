1ab45c85cf90d5544d1fd9bd987358ba22d787aa
Wooyang Son <sonwy102@gmail.com>
2020-12-02 21:41:35 -0500

    include search engine in navbar

--- static/js/navbar.jsx
+++ static/js/navbar.jsx
@@ -1,33 +1,41 @@
 const NavBar = (props) => {
   
   return (
-    <Navbar bg="white" variant="light" className="justify-content-between">
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Navbar.Brand href="/">
-          <Image
-            cloudName="sonwy102"
-            publicId="logo/Glow_ijb8hu.svg"
-            width="80em"
-          ></Image>
-        </Navbar.Brand>
-      </Navbar.Collapse>
+    <Navbar className="navbar-transparent">
+      {/* <Nav className="navbar-transparent"> */}
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Brand href="/">
+            <Image
+              cloudName="sonwy102"
+              publicId="logo/Glow_ijb8hu.svg"
+              width="80em"
+            ></Image>
+          </Navbar.Brand>
+          <Nav className="mr-sm-2">
+            <ProductSearch
+              searchTerms={props.searchTerms}
+              setSearchParams={props.setSearchParams}
+            />
+          </Nav>
+        </Navbar.Collapse>
 
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
-        {props.isLoggedIn ? (
-          <Nav className="mr-sm-2">
-            <Nav.Link href={`/dashboard?user=${props.isLoggedIn}`}>
-              Dashboard
-            </Nav.Link>
-            <LogOutBtn ensureLogIn={props.ensureLogIn}></LogOutBtn>
-          </Nav>
-        ) : (
-          <Nav className="mr-sm-2">
-            <Nav.Link href="/register">Sign Up</Nav.Link>
-            <LogInBtn ensureLogIn={props.ensureLogIn}></LogInBtn>
-          </Nav>
-        )}
-      </Navbar.Collapse>
-    </Navbar>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+          {props.isLoggedIn ? (
+            <Nav className="mr-sm-2">
+              <Nav.Link href={`/dashboard?user=${props.isLoggedIn}`}>
+                Dashboard
+              </Nav.Link>
+              <LogOutBtn ensureLogIn={props.ensureLogIn}></LogOutBtn>
+            </Nav>
+          ) : (
+            <Nav className="mr-sm-2">
+              <Nav.Link href="/register">Sign Up</Nav.Link>
+              <LogInBtn ensureLogIn={props.ensureLogIn}></LogInBtn>
+            </Nav>
+          )}
+        </Navbar.Collapse>
+      </Navbar>
+    // </Navbar>
   );
 }
