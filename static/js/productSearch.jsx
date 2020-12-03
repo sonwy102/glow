b4c03c5c3da8a517d16dc44be01f56efbff43344
Wooyang Son <sonwy102@gmail.com>
2020-12-02 21:41:47 -0500

    include search engine in navbar

--- static/js/productSearch.jsx
+++ static/js/productSearch.jsx
@@ -11,58 +11,32 @@ const ProductSearch = (props) => {
   const handleSubmit = (evt) => {
     evt.preventDefault();
     history.push(
-      `/productsearch?category=${props.category}&term=${props.searchTerms}`
+      `/productsearch?term=${props.searchTerms}`
     );
     
     //TODO: reset searchState after user submits form
-    // setSearchState({
-    //   'search-category': '',
-    //   'product-search': null
-    // });
-    // console.log(searchState)
   }
 
   return (
     <div>
-      <form onSubmit={handleSubmit} className="product-search-form">
-        <div className="form-group">
-          <label htmlFor="search-category" className="sr-only">
-            Search Category
-          </label>
-          {/* ways to make this dropdown menu better? */}
-          <select
-            id="search-category"
-            name="search-category"
-            onChange={handleInputChange}
-            required
-          >
-            <option value="DEFAULT" disabled selected>
-              I'm looking for...
-            </option>
-            <option value="Product">Product</option>
-            <option value="Brand">Brand</option>
-            <option value="Ingredient">Ingredient</option>
-          </select>
-        </div>
+      <Form inline className="product-search-form" onSubmit={handleSubmit}>
+        <Form.Label htmlFor="search-field" className="sr-only">
+          Product Search
+        </Form.Label>
+        <Form.Control
+          name="product-search"
+          className="mb-2 mr-sm-2"
+          id="search-field"
+          placeholder="Search for a product"
+          required
+          onChange={handleInputChange}
+        >
+        </Form.Control>
 
-        <div className="form-group">
-          <label htmlFor="search-field" className="sr-only">
-            Product Search
-          </label>
-          <input
-            name="product-search"
-            className="form-control input-lg"
-            placeholder="Product name, brand, ingredients..."
-            required
-            autoFocus
-            onChange={handleInputChange}
-          ></input>
-        </div>
-
-        <Button variant="primary" type="submit">
+        <Button type="submit" className="mb-2">
           Search
         </Button>
-      </form>
+      </Form>
     </div>
   );
 }
